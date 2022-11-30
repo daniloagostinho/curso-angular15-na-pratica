f94184ba07b2e5958418f3abdfbff2a237c9f9f0
Danilo Silva <danilo.silva@imaginebeyond.com.br>
2022-11-30 07:43:16 -0300

    Exibindo mensagem de usuario no menu

--- src/app/components/dashboard/menu/menu.component.ts
+++ src/app/components/dashboard/menu/menu.component.ts
@@ -1,10 +1,29 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { LocalstorageService } from 'src/app/services/localstorage.service';
 
 @Component({
   selector: 'app-menu',
   templateUrl: './menu.component.html',
   styleUrls: ['./menu.component.scss']
 })
-export class MenuComponent {
+export class MenuComponent implements OnInit {
+  messageHour!: string;
+  showNameUser!: string;
 
+  constructor(private localStorageService: LocalstorageService) {
+
+  }
+
+  ngOnInit() {
+    this.getNameUser();
+  }
+
+  getMessageHour(message: string) {
+    this.messageHour = message;
+  }
+
+  getNameUser() {
+    const nameUser = this.localStorageService.getLocalStorage('userInfo')
+    this.showNameUser = nameUser.name;
+  }
 }
--- src/app/components/dashboard/message-hour/message-hour.component.ts
+++ src/app/components/dashboard/message-hour/message-hour.component.ts
@@ -1,10 +1,31 @@
-import { Component } from '@angular/core';
+import { Component, EventEmitter, OnInit, Output } from '@angular/core';
 
 @Component({
   selector: 'app-message-hour',
   templateUrl: './message-hour.component.html',
   styleUrls: ['./message-hour.component.scss']
 })
-export class MessageHourComponent {
+export class MessageHourComponent implements OnInit {
+  @Output() messageHour = new EventEmitter();
 
+  ngOnInit() {
+    this.message();
+  }
+
+  message() {
+    let hour = new Date().getHours();
+    if(hour <= 5) {
+      this.messageHour.emit('Boa madrugada!')
+      return;
+    }
+    if(hour < 12) {
+      this.messageHour.emit('Bom dia!')
+      return;
+    }
+    if(hour < 18) {
+      this.messageHour.emit('Bom tarde!')
+      return;
+    }
+    this.messageHour.emit('Boa noite')
+  }
 }
