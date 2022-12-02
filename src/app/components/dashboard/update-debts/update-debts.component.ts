4a0e5485d04f1d5a9c829f986161b195f8d372ca
Danilo Silva <danilo.silva@imaginebeyond.com.br>
2022-12-02 12:48:20 -0300

    Criando formulário do modal de atualização das dívidas

--- src/app/components/dashboard/update-debts/update-debts.component.ts
+++ src/app/components/dashboard/update-debts/update-debts.component.ts
@@ -1,10 +1,46 @@
-import { Component } from '@angular/core';
+import { MAT_DIALOG_DATA } from '@angular/material/dialog';
+import { Component, Inject, OnInit } from '@angular/core';
+import { FormGroup, FormBuilder, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-update-debts',
   templateUrl: './update-debts.component.html',
   styleUrls: ['./update-debts.component.scss']
 })
-export class UpdateDebtsComponent {
+export class UpdateDebtsComponent implements OnInit {
+  constructor(
+    private fb: FormBuilder,
+    @Inject(MAT_DIALOG_DATA) public data: any) {
 
+  }
+  form!: FormGroup;
+  category!: string;
+
+
+  ngOnInit() {
+    console.log(this.data)
+    this.initForm();
+    this.fillData();
+  }
+
+  initForm() {
+    this.form = this.fb.group({
+      debt: [null, Validators.required],
+      category: [null, Validators.required],
+      value: [null, Validators.required],
+      epirationDate: [null, Validators.required],
+    })
+  }
+
+  fillData() {
+    if(this.data) {
+      this.category = this.data.data?.category;
+      this.form.patchValue({
+        debt: this.data.data?.debt,
+        category: this.data.data?.category,
+        value: this.data.data?.value,
+        epirationDate: this.data.data?.expirationDate,
+      })
+    }
+  }
 }
