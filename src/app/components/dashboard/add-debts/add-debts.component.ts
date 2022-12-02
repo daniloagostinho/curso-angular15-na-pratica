d72716b3d5f0b133315cddb1247349882ccec8ae
Danilo Silva <danilo.silva@imaginebeyond.com.br>
2022-12-02 09:24:57 -0300

    Criando formulario da modal de adição de dívidas

--- src/app/components/dashboard/add-debts/add-debts.component.ts
+++ src/app/components/dashboard/add-debts/add-debts.component.ts
@@ -1,10 +1,57 @@
-import { Component } from '@angular/core';
+import { Category } from './../../../interfaces/category';
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-add-debts',
   templateUrl: './add-debts.component.html',
   styleUrls: ['./add-debts.component.scss']
 })
-export class AddDebtsComponent {
 
+export class AddDebtsComponent implements OnInit {
+  form!: FormGroup;
+  category!: string;
+  categories!: Category[];
+  constructor(private fb: FormBuilder) {
+
+  }
+
+  ngOnInit() {
+    this.initForm();
+
+    this.categories = [
+      {
+        name: 'Casa',
+      },
+      {
+        name: 'Eletromesticos',
+      },
+      {
+        name: 'Saúde',
+      },
+      {
+        name: 'Entretenimento',
+      },
+      {
+        name: 'Estudo',
+      },
+      {
+        name: 'Outros',
+      },
+    ]
+  }
+
+  initForm() {
+    this.form = this.fb.group({
+      debt: [null, Validators.required],
+      category: [null, Validators.required],
+      value: [null, Validators.required],
+      exprirateDate: [null, Validators.required],
+      fixeDebt: [null],
+    })
+  }
+
+  submit() {
+    console.log(this.form)
+  }
 }
