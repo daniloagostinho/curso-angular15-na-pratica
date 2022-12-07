b2949159d99f39ea7ab1d5af8f7265d443b810ab
Danilo Silva <danilo.silva@imaginebeyond.com.br>
2022-12-07 18:24:19 -0300

    Configurando aplicação para subir  para o heroku

---
+++ server.js
@@ -0,0 +1,12 @@
+const express = require("express")
+const path = require("path")
+const app = express();
+
+const appName = "curso-angular15-na-pratica"
+
+app.use(express.static(__dirname + `/dist/${appName}`))
+app.get("/*", function (req, res) {
+  res.sendFile(path.join(__dirname + `/dist/${appName}/index.html`))
+})
+
+app.listen(process.env.PORT || 8080)
